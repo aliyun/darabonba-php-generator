623d4dc9be9e66ef07fa9219d7be8d5f1ed42a07
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-06-22 15:30:59 +0800

    add unit tests for emitter.js&helper.js

--- tests/lib.tests.js
+++ tests/lib.tests.js
@@ -1,13 +1,26 @@
 'use strict';
 
+const fs = require('fs');
 const debug = require('../src/lib/debug');
+const mm = require('mm');
 const expect = require('chai').expect;
+const Emitter = require('../src/lib/emitter');
 require('mocha-sinon');
+
+const {
+  _camelCase,
+  _subModelName,
+  _string,
+} = require('../src/lib/helper');
 
 describe('debug tests', function () {
   beforeEach(function () {
     this.sinon.stub(console, 'log');
     this.sinon.stub(process, 'exit');
+    this.sinon.stub(fs, 'mkdirSync');
+    this.sinon.stub(fs, 'writeFileSync');
+    this.sinon.stub(fs, 'appendFileSync');
+    this.sinon.stub(fs, 'existsSync');
   });
 
   it('debug.dump should be ok', function () {
@@ -60,5 +73,97 @@ describe('debug tests', function () {
       expect(console.log.calledWith('some data')).to.be.true;
       expect(process.exit.calledWith(-1)).to.be.false;
     }
+  });
+
+  it('emitter.indent should be ok', function () {
+    const emitter = new Emitter();
+    expect(emitter.indent(1)).to.be.eql('    ');
+  });
+
+  it('emitter.emit should be ok', function () {
+    const emitter = new Emitter();
+    emitter.emit('emit some string');
+    expect(emitter.output).to.be.eql('emit some string');
+  });
+
+  it('emitter.emitln should be ok', function () {
+    const emitter = new Emitter();
+    emitter.emitln('emitln some string');
+    expect(emitter.output).to.be.eql('emitln some string' + emitter.eol);
+  });
+
+  it('emitter.emits should be ok', function () {
+    const emitter = new Emitter();
+    emitter.emits(1);
+    expect(emitter.output).to.be.eql('');
+    emitter.emits(0, 'row1', 'row2');
+    expect(emitter.output).to.be.eql('row1' + emitter.eol + 'row2' + emitter.eol);
+  });
+
+  it('emitter.erase should be ok', function () {
+    const emitter = new Emitter();
+    emitter.emit('full string');
+    emitter.erase(3);
+    expect(emitter.output).to.be.eql('full str');
+  });
+
+  it('emitter.savePath should be ok', function () {
+    const emitter = new Emitter({
+      dir: '/tmp/',
+      layer: 'a.b.c',
+      filename: 'filename',
+      ext: '.tmp'
+    });
+    expect(emitter.savePath()).to.be.eql('/tmp/a/b/c/filename.tmp');
+  });
+
+  it('emitter.save should be ok', function () {
+    const emitter = new Emitter();
+    try {
+      emitter.save();
+    } catch (e) {
+      expect(e.message).to.be.eql('`option.dir` should not be empty');
+    }
+    emitter.config.dir = '/tmp/';
+    try {
+      emitter.save();
+    } catch (e) {
+      expect(e.message).to.be.eql('filename cannot be empty');
+    }
+    emitter.config.filename = 'filename';
+    emitter.config.ext = '.tmp';
+
+    emitter.emit('test');
+
+    emitter.save();
+    emitter.emit();
+    mm(fs, 'existsSync', function(filename) {
+      return true;
+    });
+    emitter.save(true);
+    mm.restore();
+
+    // show emit info
+    emitter.config.showInfo = true;
+    emitter.emit('test');
+    emitter.save();
+    const filename = emitter.savePath();
+
+    expect(filename).to.be.eql('/tmp/filename.tmp');
+  });
+
+  it('helper._camelCase should be ok', function () {
+    expect(_camelCase('test_camel_case')).to.be.eql('testCamelCase');
+  });
+
+  it('helper._subModelName should be ok', function () {
+    expect(_subModelName('test.model.name')).to.be.eql('TestModelName');
+  });
+
+  it('helper._string should be ok', function () {
+    const obj = {
+      string: 'test'
+    };
+    expect(_string(obj)).to.be.eql('test');
   });
 });
