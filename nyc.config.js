aec778a0a35f06ff5b12cc717529172608e9e168
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-06-24 10:15:10 +0800

    add nyc.config.js, coverage ignore tests dir.

---
+++ nyc.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  exclude: [
+    'coverage',
+    'self-coverage',
+    'tests/*.js',
+  ],
+  branches: 100,
+  functions: 100,
+  lines: 100,
+  statements: 100
+};
