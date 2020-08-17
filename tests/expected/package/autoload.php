3e76e4e6840eb49be7f3b7887fe87a5ddf18cf82
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-08-17 14:56:08 +0800

    Patch php generator 1. Improve emit result of autoload script 2. Add `package` test case

--- tests/expected/package/autoload.php
+++ tests/expected/package/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    $name = str_replace('${namespace}\\', '', $class);
+    $name = str_replace('Darabonba\\Package\\', '', $class);
     $file = __DIR__ . \DIRECTORY_SEPARATOR . 'src' . \DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $name) . '.php';
     if (file_exists($file)) {
         require_once $file;
