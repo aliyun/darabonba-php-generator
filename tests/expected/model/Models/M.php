798a37be757c7d7ba8fa0181c121e1ba5037d44c
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-08-20 19:30:09 +0800

    Improve php generator 1. Fix the error while map has model type item. 2. `subModel_or_moduleModel` is retained for compatibility with older parsers.

--- tests/expected/model/Models/M.php
+++ tests/expected/model/Models/M.php
@@ -5,10 +5,17 @@ namespace Tea\PHP\Tests\Models;
 
 use AlibabaCloud\Tea\Model;
 
+use Tea\PHP\Tests\Models\M\subM;
+
 class M extends Model {
-    public function validate() {}
+    public function validate() {
+        Model::validateRequired('subM', $this->subM, true);
+    }
     public function toMap() {
         $res = [];
+        if (null !== $this->subM) {
+            $res['subM'] = null !== $this->subM ? $this->subM->toMap() : null;
+        }
         return $res;
     }
     /**
@@ -17,6 +24,14 @@ class M extends Model {
      */
     public static function fromMap($map = []) {
         $model = new self();
+        if(isset($map['subM'])){
+            $model->subM = subM::fromMap($map['subM']);
+        }
         return $model;
     }
+    /**
+     * @var subM
+     */
+    public $subM;
+
 }
