ae516b9c0025920ec0edd748a16934d9757b0876
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2021-03-29 16:44:55 +0800

    test: number operation

---
+++ tests/expected/number/Client.php
@@ -0,0 +1,28 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+namespace Tea\PHP\Tests;
+
+class Client {
+
+    /**
+     * @param string[] $args
+     * @return void
+     */
+    public static function main($args){
+        $a = "123";
+        $b = (int)$a;
+        $c = (double)$a;
+        $d = (double)$a;
+        $e = $b;
+        $f = (int)$a;
+        $g = $e + $f;
+        $g = $e - $f;
+        $g = $e * $f;
+        $z = $e / $f;
+        $h = $e > $f;
+        $h = $e >= $f;
+        $h = $e < $f;
+        $h = $e <= $f;
+    }
+}
